3a42021f6e9dcaa10f97d3e565d2ecec4759055b
laharireddy6453 <145620551+laharireddy6453@users.noreply.github.com>
2023-11-01 11:08:33 -0500

    adding the login function to website

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="contact.php">Feedback</a>
+            <a href="shop.php">shop</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="login.php">login</a>
+            <a href="register.php">register</a>
+            <a href="orders.php">my orders</a>
+            <a href="cart.php">my cart</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> +1 940-843-6547 </p>
+            <p> <i class="fas fa-phone"></i> +1 940-843-6548 </p>
+            <p> <i class="fas fa-envelope"></i> test@my.unt.edu </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Union Circle,Denton 76201 </p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="https://www.facebook.com/northtexas/"><i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="https://twitter.com/UNTsocial?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor"><i class="fab fa-twitter"></i>twitter</a>
+            <a href="https://www.instagram.com/UNT/"><i class="fab fa-instagram"></i>instagram</a>
+            <a href="https://www.linkedin.com/school/northtexas/"><i class="fab fa-linkedin"></i>linkedin</a>
+        </div>
+
+    </div>
+
+    <div class="credit">&copy; copyright @ <?php echo date('Y'); ?> by <span>wedz</span> </div>
+
+</section>
